1319681d5de6bfdb490523deb82c7f3d9708654a
tonygcurran <62523322+tonygcurran@users.noreply.github.com>
2021-10-11 09:12:04 +1100

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -19,7 +19,7 @@ function setup() {
     
   w = color(255);
   angleMode(DEGREES);
-    st = random(width/20);
+    st = random(width/30);
 }
 
 function draw() {
@@ -146,7 +146,7 @@ function setup() {
   ralpha = random(100);
   w = color(255);
   angleMode(DEGREES);
-    st = random(width/20);
+    st = random(width/30);
 }
 }  
 
